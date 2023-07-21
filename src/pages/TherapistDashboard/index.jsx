39ffa19388471f9f3ded23e3e784f20006544070
Andy Cockroft <acockroft1994@gmail.com>
2023-07-21 15:56:40 +0200

    therapist logout now in therapist  dashboard

--- src/pages/TherapistDashboard/index.jsx
+++ src/pages/TherapistDashboard/index.jsx
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import axios from "axios";
+import { Button, Input } from "@fluentui/react-components";
+import { Link } from "react-router-dom";
+import { TherapistAuthContext } from "../../context/therapistAuth.context";
 
 function TherapistDashboard() {
+
+  const { logOutTherapist } = useContext(TherapistAuthContext);
   return (
     <div>
+      <nav>
+        <Button onClick={logOutTherapist}>Logout</Button>
+      </nav>
       <div>
         <h1>Profile</h1>
         <p>Welcome back (username).</p>
